970b6756c11ae2feee6389edd77246be290bd784
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2020-06-07 17:54:16 +0300

    fix compilation issue

--- shared/src/commonMain/kotlin/com/krossovochkin/kweather/shared/common/presentation/BaseViewModel.kt
+++ shared/src/commonMain/kotlin/com/krossovochkin/kweather/shared/common/presentation/BaseViewModel.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
